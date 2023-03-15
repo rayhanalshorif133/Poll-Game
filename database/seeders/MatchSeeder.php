20482d2fdb6c29549d1217b460396b878cb71dd8
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-03-15 16:04:28 +0600

    seeder cng

--- database/seeders/MatchSeeder.php
+++ database/seeders/MatchSeeder.php
@@ -52,5 +52,32 @@ class MatchSeeder extends Seeder
             'created_by' => 1,
             'updated_by' => 1,
         ]);
+
+
+        Matches::create([
+            'tournament_id' => 2,
+            'team1_id' => 3,
+            'team2_id' => 1,
+            'title' => 'Match 4',
+            'start_date_time' => '2021-02-19 09:00:00',
+            'end_date_time' => '2021-02-19 10:00:00',
+            'status' => 'active',
+            'description' => 'This is a test match',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
+
+        Matches::create([
+            'tournament_id' => 2,
+            'team1_id' => 3,
+            'team2_id' => 2,
+            'title' => 'Match 5',
+            'start_date_time' => '2021-02-19 09:00:00',
+            'end_date_time' => '2021-02-19 10:00:00',
+            'status' => 'active',
+            'description' => 'This is a test match',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
     }
 }
--- database/seeders/PollSeeder.php
+++ database/seeders/PollSeeder.php
@@ -50,5 +50,92 @@ class PollSeeder extends Seeder
             'created_by' => 1,
             'updated_by' => 1,
         ]);
+
+        Poll::create([
+            'match_id' => 3,
+            'question' => 'What does ice become when you heat it?',
+            'images' => json_encode([
+                'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+            ]),
+            'option_1' => 'Water',
+            'option_2' => 'Steam',
+            'option_3' => 'Ice',
+            'option_4' => 'None of the above',
+            'answer' => 'option_1',
+            'point' => 5,
+            'status' => 'active',
+            'description' => 'This is a description',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
+
+        Poll::create([
+            'match_id' => 3,
+            'question' => 'From where in the universe do we get solar energy?',
+            'images' => json_encode([
+                'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+            ]),
+            'option_1' => 'Sun',
+            'option_2' => 'Moon',
+            'answer' => 'option_1',
+            'point' => 5,
+            'status' => 'active',
+            'description' => 'This is a description',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
+
+        Poll::create([
+            'match_id' => 1,
+            'question' => 'What is the capital of India?',
+            'images' => json_encode([
+                'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+            ]),
+            'option_1' => 'Delhi',
+            'option_2' => 'Mumbai',
+            'option_3' => 'Kolkata',
+            'option_4' => 'Chennai',
+            'answer' => 'option_1',
+            'point' => 5,
+            'status' => 'active',
+            'description' => 'This is a description',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
+
+        Poll::create([
+            'match_id' => 2,
+            'question' => 'What is the capital of Bangladesh?',
+            'images' => json_encode([
+                'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+            ]),
+            'option_1' => 'Dhaka',
+            'option_2' => 'Chittagong',
+            'option_3' => 'Khulna',
+            'answer' => 'option_1',
+            'point' => 5,
+            'status' => 'active',
+            'description' => 'This is a description',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
+
+        Poll::create([
+            'match_id' => 3,
+            'question' => 'What is the capital of Pakistan?',
+            'images' => json_encode([
+                'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+            ]),
+            'option_1' => 'Islamabad',
+            'option_2' => 'Karachi',
+            'option_3' => 'Lahore',
+            'option_4' => 'Peshawar',
+            'answer' => 'option_1',
+            'point' => 5,
+            'status' => 'active',
+            'description' => 'This is a description',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
     }
 }
