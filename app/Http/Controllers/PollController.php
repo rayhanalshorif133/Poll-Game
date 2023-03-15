01087ae02205b3cc236520693138fbcb240d65ca
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-03-15 11:55:02 +0600

    admin panel poll create

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -46,11 +46,92 @@ class PollController extends Controller
         $navItem = 'poll-create';
         $matches = Matches::select()->get();
         $optionTypes = ['Text', 'Image'];
-        return view('poll.create', compact('navItem', 'matches', 'optionTypes'));
+        $answerOptions = [
+            ['value' => 'option_1', 'name' => 'Option 1'],
+            ['value' => 'option_2', 'name' => 'Option 2']
+        ];
+        return view('poll.create', compact('navItem', 'matches', 'optionTypes', 'answerOptions'));
     }
 
     public function store(Request $request)
     {
-        dd($request->all());
+
+        $request->validate([
+            'match_id' => 'required',
+            'question' => 'required',
+            'option_type' => 'required',
+            'option1' => 'required',
+            'option2' => 'required',
+            'answer' => 'required',
+        ]);
+
+
+        $poll = new Poll();
+        $poll->match_id = $request->match_id;
+        $poll->question = $request->question;
+
+        if ($request->file('question_images')) {
+            foreach ($request->file('question_images') as $image) {
+                $imageName = time() . '.' . $image->extension();
+                $image->move(public_path('storage/images/questions'), $imageName);
+                $imageName = 'storage/images/questions/' . $imageName;
+                $data[] = $imageName;
+            }
+            $poll->images = json_encode($data);
+        }
+        $poll->option_type = $request->option_type == 1 ? 'image' : 'text';
+        // option type 0 = text
+        // option type 1 = image
+        if ($request->option_type == 1) {
+            for ($index = 1; $index <= 4; $index++) {
+                if ($request->file('option' . $index)) {
+                    $getOption = 'option' . $index;
+                    $pullOption = 'option_' . $index;
+                    $imageName = time() . '.' . $request->$getOption->extension();
+                    $request->$getOption->move(public_path('storage/images/questions'), $imageName);
+                    $imageName = 'storage/images/questions/' . $imageName;
+                    $poll->$pullOption = $imageName;
+                }
+            }
+        } else {
+            for ($index = 1; $index <= 4; $index++) {
+                $getOption = 'option' . $index;
+                $pullOption = 'option_' . $index;
+                if ($request->$getOption) {
+                    $poll->$pullOption = $request->$getOption;
+                }
+            }
+        }
+        $poll->answer = $request->answer ? $request->answer : null;
+        $poll->status = $request->status ? $request->status : 'Active';
+        $poll->description = $request->description ? $request->description : null;
+        $poll->created_by = auth()->user()->id;
+        $poll->updated_by = auth()->user()->id;
+
+        $poll->save();
+        Session::flash('success', 'Poll created successfully.');
+        Session::flash('class', 'success');
+        return redirect()->back();
+        // return redirect()->route('poll.index');
+
+        /*
+
+        'match_id',
+        'question',
+        'images',
+        'option_type',
+        'option_1',
+        'option_2',
+        'option_3',
+        'option_4',
+        'answer',
+        'status',
+        'description',
+        'created_by',
+        'updated_by',
+
+
+
+        */
     }
 }
--- resources/views/poll/create.blade.php
+++ resources/views/poll/create.blade.php
@@ -70,7 +70,7 @@
                                     <option value="{{$match->id}}">{{$match->title}}</option>
                                     @endforeach
                                 </select>
-                            </div>
+                        </div>
                     </div>
                     <div class="form-group">
                         <label for="question" class="required">Question?</label>
@@ -91,6 +91,7 @@
                             </div>
                         </div>
                     </div>
+                    <span id="append_question_image"></span>
                     <div class="form-group">
                         <label for="name" class="required">Choise option's type</label>
                         <div class="row">
@@ -122,6 +123,19 @@
                     </span>
                     <button type="button" class="btn btn-sm btn-outline-mahogany mb-3 addNewOption"><i
                                     class="fa-solid fa-plus"></i> Add Option</button>
+                    <div class="form-group">
+                        <label for="name" class="required">Choise the Answer</label>
+                        <div class="row">
+                            <div class="col-md-11">
+                                <select class="form-control" name="answer" id="answer">
+                                    <option value="" selected disabled>Select Answer</option>
+                                    @foreach($answerOptions as $answerOption)
+                                    <option value="{{$answerOption['value']}}">{{$answerOption['name']}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                    </div>
+                </div>
                 <div class="card-footer">
                     <button type="submit" class="btn btn-mahogany">Submit</button>
                 </div>
@@ -129,14 +143,44 @@
         </div>
     </div>
 </div>
+
+
 @endsection
 
 @push('js')
 <script>
     $(function(){
+        addNewQuestionImageHandler();
         optionTypeHandler();
         addNewOptionHandler();
     });
+
+
+    addNewQuestionImageHandler = () => {
+        $(".addNewQuestionImage").click(function () {
+            let image = "";
+             image = `
+            <div class="form-group">
+                <label for="question_images" class="optional">Question Image</label>
+                <div class="row">
+                    <div class="col-md-11">
+                        <input type="file" class="form-control" name="question_images[]">
+                    </div>
+                    <div class="col-md-1 text-left">
+                        <button type="button" class="btn btn-sm btn-outline-danger removeNewQuestion"><i class="fa-solid fa-minus"></i></button>
+                    </div>
+                </div>
+            </div>`;
+            $("#append_question_image").append(image);
+            removeNewQuestionHandler();
+        });
+    }
+
+    removeNewQuestionHandler = () => {
+        $(".removeNewQuestion").click(function () {
+            $(this).parent().parent().parent().remove();
+        });
+    }
 
     optionTypeHandler = () => {
         $("#option_type").change(function(){
@@ -145,15 +189,25 @@
             $("#append_option").html('');
             type == 1 ? type = 'file' : type = 'text';
             let image = '';
+            let answerOption = '<option value="" selected disabled>Select Answer</option>';
             for (let index = 1; index <= 2; index++) {
                 image += `
                 <div class="form-group">
                     <label for="option${index}" class="required">Option ${index}</label>
-                    <input type="${type}" class="form-control" name="option${index}" id="option${index}" placeholder="Enter your option">
+                    <div class="row">
+                        <div class="col-md-11">
+                            <input type="${type}" class="form-control" name="option${index}" id="option${index}" placeholder="Enter your option">
+                        </div>
+                    </div>
                 </div>`;
+                answerOption += `
+                <option value="option_${index}">Option ${index}</option>
+                `;
             }
             $("#append_option").append(image);
             $(".addNewOption").show();
+
+            $("#answer").html(answerOption);
         });
     }
 
@@ -162,7 +216,7 @@
             let type = $("#option_type").val();
             type = parseInt($("#option_type").val());
             type == 1 ? type = 'file' : type = 'text';
-            let index = $(".form-group").length - 3;
+            let index = $(".form-group").length - 4;
             let option = `
             <div class="form-group">
                 <label for="option${index}" class="optional">Option ${index}</label>
@@ -178,6 +232,9 @@
             </div>`;
             $("#append_option").append(option);
 
+
+            appendAnswerOptionHandler(index);
+
             if(index == 4){
                 $(".addNewOption").hide();
             }
@@ -186,7 +243,20 @@
         $(document).on("click", ".removeNewOption", function () {
             $(this).closest(".form-group").remove();
             $(".addNewOption").show();
+
+            let index = $(".form-group").length - 4;
+            $("#answer option[value='option_"+index+"']").remove();
         });
+
+    }
+
+
+    appendAnswerOptionHandler = (index) => {
+        console.log(index);
+        let answerOption = `
+            <option value="option_${index}">Option ${index}</option>
+        `;
+        $("#answer").append(answerOption);
     }
 
 
