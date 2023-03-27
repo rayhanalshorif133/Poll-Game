00f1beaeac08500a43d9e7854a917a9d673c979a
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-03-27 11:45:32 +0600

    subcription added

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,13 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscription;
+use App\Models\Tournament;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\Facades\DataTables;
 
 class SubscriptionController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $navItem = 'subscription-list';
+        if ($request->ajax()) {
+            // DISTINCT
+            $data = Subscription::select('match_id')
+                ->distinct('match_id')
+                ->with('match')
+                ->get();
+            return DataTables::of($data)->addIndexColumn()
+                ->addColumn('tournament', function ($row) {
+                    $tournament = Tournament::find($row->match->tournament_id);
+                    return $tournament->name;
+                })
+                ->addColumn('subscription', function ($row) {
+                    $countOfSubscription = Subscription::select('match_id')
+                        ->where('match_id', $row->match_id)
+                        ->count();
+                    return $countOfSubscription;
+                })
+                ->addColumn('action', function ($row) {
+                    return true;
+                })
+                ->make(true);
+        }
 
         return view('subscription.index', compact('navItem'));
     }
--- resources/views/subscription/index.blade.php
+++ resources/views/subscription/index.blade.php
@@ -15,12 +15,12 @@
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-bordered poll_datatable w-100">
+                    <table class="table table-bordered subscription_datatable w-100">
                         <thead>
                             <tr>
                                 <th>#</th>
-                                <th>Tournament Name</th>
                                 <th>Match</th>
+                                <th>Tournament Name</th>
                                 <th>Count Of Subscription</th>
                                 <th>Action</th>
                             </tr>
@@ -38,40 +38,16 @@
 <script type="text/javascript">
     var table = "";
     $(function() {
-        // handleDataTable();
-        handleSelectedMatch();
+        handleDataTable();
     });
 
 
-    handleSelectedMatch = () => {
-        $(document).on('change', '#match_id', function() {
-            let match_id = $(this).val();
-            let timeDiff = $(this).find(':selected').data('timediff');
-            $('#match_day').empty();
-            $('#match_day').append(`<option value="" selected disabled>Select Day</option>`);
-            for (let day = 1; day <= timeDiff; day++) {
-                $('#match_day').append(`<option value="${day}">Day-${day}</option>`);
-            }
-            // table.destroy();
-            // handleDataTable(match_id);
-        });
 
-        $(document).on('change', '#match_day', function() {
-            console.log($(this).val());
-            let match_id = $('#match_id').val();
-            let day = $(this).val();
-            table.destroy();
-            // handleDataTable(match_id, day);
-        });
-    }
-
-    handleDataTable = (match_id = null, day = null) =>{
-        let url = match_id? `/admin/poll/${match_id}/` : "/admin/poll/";
-        url = day? `${url}${day}` : url;
-        table = $('.poll_datatable').DataTable({
+    handleDataTable = () =>{
+        table = $('.subscription_datatable').DataTable({
             processing: true,
             serverSide: true,
-            ajax: url,
+            ajax: "{{ route('subscription.index') }}",
             columns: [{
                     render: function(data, type, row) {
                         return row.DT_RowIndex;
@@ -87,71 +63,23 @@
                 },
                 {
                     render: function(data, type, row) {
-                        return "Day-" + row.day;
+                        let name = `<a href="/tournament/${row.match.tournament_id}/view">${row.tournament}</a>`;
+                        return name;
                     },
                     targets: 0,
                 },
                 {
                     render: function(data, type, row) {
-                        return row.question;
+                        return row.subscription;
                     },
                     targets: 0,
                 },
                 {
                     render: function(data, type, row) {
-                        if(row.option_type == "text")
-                        {
-                            if(row.answer == 'option_1'){
-                                return row.option_1;
-                            }else if(row.answer == 'option_2'){
-                                return row.option_2;
-                            }else if(row.answer == 'option_3')
-                            {
-                                return row.option_3;
-                            }else if(row.answer == 'option_4')
-                            {
-                                return row.option_4;
-                            }
-                            else{
-                                return 'Not Set';
-                            }
-                        }else{
-                            for (let index = 1; index <= 4; index++) {
-                                let isAnswerOption = row.answer == 'option_'+index ? true : false;
-                                let option = index == 1 ? row.option_1 : index == 2 ? row.option_2 : index == 3 ? row.option_3 : row.option_4;
-                                if(isAnswerOption){
-                                    let image = `
-                                    <a class="example-image-link" href="${option}" data-lightbox="example-set"
-                                        data-title="Click the right half of the image to move forward.">
-                                        <img class="example-image p-2 bd-3" height="75"
-                                            width="75" src="${option}" alt="" />
-                                    </a>
-                                    `;
-                                    return image;
-                                }
-                                else{
-                                    return 'Not Set';
-                                }
-                            }
-                        }
-                    },
-                    targets: 0,
-                },
-                {
-                    render: function(data, type, row) {
-                        return row.created_by.name;
-                    },
-                    targets: 0,
-                },
-                {
-                    render: function(data, type, row) {
-                        return row.updated_by.name;
-                    },
-                    targets: 0,
-                },
-                {
-                    render: function(data, type, row) {
-                        return getButtons("/poll/admin", row.id);
+                        let action = `<a href="/subscription/${row.id}/view" class="btn btn-sm btn-outline-green" data-toggle="tooltip" data-placement="top" title="View">
+                                        <i class="fa fa-eye" aria-hidden="true"></i>
+                                    </a>`;
+                        return action;
                     },
                     targets: 0,
                 },
