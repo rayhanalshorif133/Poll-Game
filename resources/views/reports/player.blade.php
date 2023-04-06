16909e8e4361ca84a89e02ced6652c4687c0dbd2
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-06 11:16:03 +0600

    header in player info

--- resources/views/reports/player.blade.php
+++ resources/views/reports/player.blade.php
@@ -5,20 +5,28 @@
     .breadcrumb{
         background-color: transparent!important;
     }
+    .content-header h1 {
+        font-size: 1.8rem;
+        margin: 0;
+    }
 </style>
 @endsection
 
 @section('content')
-<div class="row mb-2">
-    <div class="col-sm-6">
-        <h1 class="m-0">Player's Report</h1>
-    </div>
-    <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{route('user.dashboard')}}">Home</a></li>
-            <li class="breadcrumb-item active">Player's Report</li>
-        </ol>
-    </div>
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Player's Report</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{route('user.dashboard')}}">Home</a></li>
+                    <li class="breadcrumb-item active">Player's Report</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
 </div>
 <div class="col-md-12">
     <div class="card">
@@ -270,7 +278,6 @@
                         </div>
                     </form>
                 </div>
-
             </div>
         </div>
     </div>
